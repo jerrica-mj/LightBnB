8befc4c97442fcde7039acd4b23e8dd338c9bdd8
JJ <jerrica.mj@gmail.com>
2021-02-12 05:23:05 +0000

    Add more activity code.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+// connect to our database
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
 
 /// Users
 
@@ -67,11 +77,27 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+  const queryString = `
+  SELECT *
+  FROM properties
+  LIMIT $1;
+  `;
+  const values = [limit];
+
+  pool.query(queryString, values)
+    .then(res => {
+      console.log(res.rows);
+    })
+    .catch(err => console.error('query error', err.stack));
+
+
+
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
 }
 exports.getAllProperties = getAllProperties;
 
